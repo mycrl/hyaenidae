bb81572038c177d000ad9335f5c4b6fef8103ece
mycrl <lepidodendraceae@gmail.com>
2026-05-17 14:30:54 +0800

    refactor: move logo to public

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import BrowserDemo from "./components/BrowserDemo/BrowserDemo.tsx";
-import logo from "./assets/logo.svg";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 import { FaGithub } from "react-icons/fa6";
+import logo from "../public/logo.svg";
 
 export default function App() {
     return (
